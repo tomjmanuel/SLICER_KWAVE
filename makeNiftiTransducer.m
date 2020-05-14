508167829a410086b31073671dfb302e64a662b5
Tom Manuel <tom.j.manuel@gmail.com>
2020-05-14 15:59:55 -0500

    fixed make xdcr nifti

--- makeNiftiTransducer.m
+++ makeNiftiTransducer.m
@@ -1,21 +1,25 @@
 % make nifti transducer file 
 % input makeSphericalRandom
 % uses makemultibowl (kwave)
+% after writing nifti, you must load it into slicer and adjust 
+%   voxel sizes
 
-load('transducer_32elem_R75_D75.mat')
+load('transducer_64elem_R50_D50.mat')
 
-radius_mm = 75;
-diameter_mm = 6; %element diameter not transducer diameter
-nE = 32; % n elements
+radius_mm = 50;
+diameter_mm = 5; %element diameter not transducer diameter
+nE = 64; % n elements
 
-usf = 2; % grid usf
-padding = 10;
+usf = 8; % grid usf
+padding = 10; % padding in all 3 dim
+zpaddingextra = 30; % optional extra z padding
 
 % A has element positions in mm
 D = ceil(range(A))*usf;
 fL = round(D/2);
 D = D+padding;
 fL(3)=radius_mm*usf+padding/2; % set z focus at 1;
+D(3) = fL(3)+padding+zpaddingextra;
 
 diam_pix = diameter_mm*usf;
 if mod(diam_pix,2)==0
@@ -34,9 +38,11 @@ A = round(A);
 [bowls,labels] = makeMultiBowl(D,A,radius_mm*usf,diam_pix,fL);
 
 %mark focus in bowls
-bs=2;
-bowls(fL(1)+bs:fL(1)-bs,fL(2)+bs:fL(2)-bs,fL(3)+bs:fL(3)-bs)=1;
+% make grid big enough to encomposs focus + padding
 
-niftiwrite(labels,'trans_32elem_R75_D75_labeled.nii');
 
-niftiwrite(bowls,'trans_32elem_R75_D75.nii');
+bs=5;
+bowls(fL(1)-bs:fL(1)+bs,fL(2)-bs:fL(2)+bs,fL(3)-bs:fL(3)+bs)=1;
+
+niftiwrite(labels,'xdcr_64elem_R50_D50_vox125um_labeled.nii');
+niftiwrite(bowls,'xdcr_64elem_R50_D50_vox125um.nii');
