3806f4614237e136070b1c6490407c8f7454a6a3
Tom Manuel <tom.j.manuel@gmail.com>
2020-07-29 13:30:51 -0500

    added absorption to getAcousticProperties.m

--- getAcousticProperties.m
+++ getAcousticProperties.m
@@ -1,7 +1,5 @@
 %% getAcoustic properties
-%4/16/20
-%TODO: add attenuation, datacasting
-
+%7/29/20
 function medium = getAcousticProperties(CT)
     %set water equal to air
     CT(CT<0)=0; 
@@ -27,4 +25,21 @@ function medium = getAcousticProperties(CT)
 
     medium.sound_speed = c_water.*ones(size(CT));
     medium.sound_speed(bone_i) = c_water+(c_skull_max-c_water)*rel_CT(bone_i);
+
+        %%%%%%%%%%%%%%%% absorption
+    % from manual: alpha_coeff units: [dB/Mhz/cm] (page 32)
+    %              alpha_power: scales alpha with frequency (just set to 1) we
+    %                   are close to 1Mhz anyway
+    %                   can't actually set to 1 but set close to 1
+    % Ref 2 explains this well
+
+    %from ref 1: bone gives absoprtion of 2.7 dB/Mhz/cm longitudinal 
+    %            and 5.4 dB/Mhz/cm shear...
+    % i will create binary mask of bone and set alpha to 8 dB/Mhz/cm within
+    % mask, 0.2 dB/Mhz/cm everywhere else
+
+
+    medium.alpha_power = 1.1; % from aubry and commonly used
+    medium.alpha_coeff = 0.2.*ones(size(kgrid.x));
+    medium.alpha_coeff(bone_i) = 2.7+5.4;
 end
